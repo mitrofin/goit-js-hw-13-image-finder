634b9aad39550a18cb226e28d296524369a57f33
mitrofin <mitrofn@gmail.com>
2020-11-23 00:44:26 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,103 @@
 import './styles.css';
+import cardTmpl from './templates/images-card.hbs';
+import apiService from './js/apiService.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import "material-design-icons/iconfont/material-icons.css";
+
+
+const { alert, info, error } = require('@pnotify/core');
+
+const debounce = require('debounce');
+const api = new apiService();
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    containerList: document.querySelector('.gallery'),
+    clickButton:document.getElementById('button'),
+}
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
+refs.clickButton.addEventListener('click', onLoadMore);
+
+function onSearch(event) {
+    event.preventDefault();
+    clearResult();
+    clearPage();
+    api.img = event.target.value;
+    if (api.img.length === 0 || api.img === " ") {
+        clearResult();
+        const myInfo = info({
+            text:
+                "Спробуйте ще раз!",
+             delay: 700,
+        });
+        return;
+    } 
+    
+    api.fetchApi().then(renderCard)
+        .catch(onFetchError);
+     
+}
+
+function renderCard({hits}) {
+    console.log(hits);
+    const markUp = cardTmpl(hits);
+    appendMarkUp(markUp);
+}
+function appendMarkUp(html) {
+    refs.containerList.insertAdjacentHTML('beforeend', html);
+}
+function onLoadMore() {
+    api.fetchApi().then(renderCard)
+    api.inrementPage()
+}
+
+function onFetchError(error) {
+    console.log('catch');
+    showError();
+}
+
+/* function appendCountries(country) {
+    const markUp = countryListTmpl(country);
+    console.log(markUp);
+    const markUpName = countryNameTmpl(country);
+    console.log(markUpName);
+
+    if (country.length === 1) {
+        return (refs.containerList.insertAdjacentHTML('beforeend', markUp));   
+    }
+
+    if (country.length > 1) {
+        return (refs.containerList.insertAdjacentHTML('beforeend', markUpName))
+    }
+} */
+
+function showError() {
+    const myError = error({      
+    text:
+            "Нічого не знайдено! ",
+        delay: 700,
+    });
+   
+}
+function showAlert() {
+    const myAlert = alert({
+    text:
+            "Багато співпадінь.Уточіть пошук! Використовуйте ENG",
+        delay: 700,
+    });
+    
+}
+function clearResult() {
+        refs.searchForm.value = "";
+    }
+
+function clearPage() {
+        refs.containerList.innerHTML = "";
+}
+
+
+
+
+
+
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,1 +1,45 @@
-/* https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ */
+
+export default class apiService {
+    constructor() {
+        this.searchImg = 1;
+        this.startPage = 1;
+        this.per_page = 12;
+        /* this.MY_KEY = '19219612-10b40d6746041ffd691397b9a'; */
+    }
+    
+  
+    
+    fetchApi() {
+
+        const MY_KEY = '19219612-10b40d6746041ffd691397b9a';
+
+        return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchImg}&page=${this.startPage}&per_page=${this.per_page}&key=${MY_KEY}`)
+            .then(response => response.json())
+            .then(e => {
+                console.log(e);
+                this.inrementPage();
+                return e;
+            }
+                
+            );
+    }
+
+    inrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get img() {
+        return this.searchImg;
+    }
+    set img(newImg) {
+        this.searchImg = newImg;
+    }              
+    
+}
+
+
+    
